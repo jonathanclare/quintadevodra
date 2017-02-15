bd3bdc885e80e27d252caaae33787ec66d07133a
JC <jonathan.david.clare@gmail.com>
2017-02-15 17:47:31 +0000

    Updated text

--- js/main.js
+++ js/main.js
@@ -1,112 +1,73 @@
-if (!Modernizr.flexbox) {
+// If flexbox css is not supported load the basic page.
+if (!Modernizr.flexbox) 
+{
     window.location = './basic.html';
 } 
 
-/*
+// Called when page has loaded.
 window.onload = function () 
 { 
-    onScroll();
-    on(window, 'scroll resize', debounce(onScroll));
+    addImagesFromPageToSlideshow();
 } 
-
-function onScroll()
-{
-    lazyLoadImages();
-}
-
-// Lazy Load slides.
-function lazyLoadSlides () 
-{
-    [].forEach.call(document.querySelectorAll('.modal img[data-src]'), function(img) 
-    {
-        img.setAttribute('src', img.getAttribute('data-src'));
-        img.onload = function() 
-        {
-            img.removeAttribute('data-src');
-        }
-    });
-}
-
-// Lazy Load slides images.
-function lazyLoadImages () 
-{
-    var buffer = viewportHeight(); // The height above the image at which it starts to load - so images start to load just before scrolled into view.
-
-    [].forEach.call(document.querySelectorAll('.img-lazy-load[data-src]'), function(elt) 
-    {
-        if((offset(elt) - buffer) <= pageBottomOffset())
-        {
-            var src = elt.getAttribute('data-src');
-            if (elt.nodeName == 'IMG')
-            {
-                elt.setAttribute('src', src);
-                elt.onload = function() 
-                {
-                    elt.removeAttribute('data-src');
-                    addClass(elt, 'img-lazy-load-complete');
-                }
-            }
-            else
-            {
-                var img = document.createElement('img'); 
-                img.setAttribute('src', src);
-                img.onload = function() 
-                {
-                    elt.style.backgroundImage = 'url(' + src + ')';
-                    elt.removeAttribute('data-src');
-                    addClass(elt, 'img-lazy-load-complete');
-                }
-            }
-
-            // Add click to open slideshow.
-            (function (_src) {
-                on(elt, 'click', function(e)
-                {
-                    openSlideShow(_src);
-                });
-            })(src);
-        }
-    });
-}
-*/
 
 // Slideshow.
 function openSlideShow(src)
 {
     addClass(document.body, 'hide-scrollbars');
-    var slideshow = document.querySelector('#slideshow');
-
+    var slideshow = document.querySelector('.slideshow');
     if (src !== undefined)
     {
-        var activeSlide = slideshow.querySelector('.modal-slide-active');
+        var activeSlide = slideshow.querySelector('.slideshow-slide-active');
         var nextSlide = slideshow.querySelector('img[src="'+src+'"], img[data-src="'+src+'"]').parentElement.parentElement;
-        addClass(nextSlide, 'modal-slide-active');
-        removeClass(activeSlide, 'modal-slide-active');
+        addClass(nextSlide, 'slideshow-slide-active');
+        removeClass(activeSlide, 'slideshow-slide-active');
     }
-
-    addClass(slideshow, 'modal-active');
+    addClass(slideshow, 'slideshow-active');
 }
 function closeSlideShow()
 {
     removeClass(document.body, 'hide-scrollbars');
-    var slideshow = document.querySelector('#slideshow');
-    removeClass(slideshow, 'modal-active');
+    var slideshow = document.querySelector('.slideshow');
+    removeClass(slideshow, 'slideshow-active');
 }
 function nextSlide()
 {
-    var slideshow = document.querySelector('#slideshow');
-    var activeSlide = slideshow.querySelector('.modal-slide-active');
+    var slideshow = document.querySelector('.slideshow');
+    var activeSlide = slideshow.querySelector('.slideshow-slide-active');
     var nextSlide = (activeSlide.nextElementSibling != null ? activeSlide.nextElementSibling : activeSlide.parentNode.firstElementChild);
-    addClass(nextSlide, 'modal-slide-active');
-    removeClass(activeSlide, 'modal-slide-active');
+    addClass(nextSlide, 'slideshow-slide-active');
+    removeClass(activeSlide, 'slideshow-slide-active');
 }
 function prevSlide()
 {
-    var slideshow = document.querySelector('#slideshow');
-    var activeSlide = slideshow.querySelector('.modal-slide-active');
+    var slideshow = document.querySelector('.slideshow');
+    var activeSlide = slideshow.querySelector('.slideshow-slide-active');
     var prevSlide = (activeSlide.previousElementSibling != null ? activeSlide.previousElementSibling : activeSlide.parentNode.lastElementChild);
-    addClass(prevSlide, 'modal-slide-active');
-    removeClass(activeSlide, 'modal-slide-active');
+    addClass(prevSlide, 'slideshow-slide-active');
+    removeClass(activeSlide, 'slideshow-slide-active');
+}
+function addImagesFromPageToSlideshow()
+{
+    var strHtml = '';
+    [].forEach.call(document.querySelectorAll('.bg-img'), function(elt, index) 
+    {
+        var src = window.getComputedStyle(elt).backgroundImage.slice(5, -2);
+        var title = elt.getAttribute('title');
+        if (index === 0)
+            strHtml += '<div class="slideshow-slide slideshow-slide-active"><div class="slideshow-img flex-box"><img src="'+src+'" /></div><div class="slideshow-text">'+title+'</div></div>';
+        else
+            strHtml += '<div class="slideshow-slide"><div class="slideshow-img flex-box"><img src="'+src+'" /></div><div class="slideshow-text">'+title+'</div></div>';
+
+        // Add click to open slideshow.
+        (function (_src) {
+            on(elt, 'click', function(e)
+            {
+                openSlideShow(_src);
+            });
+        })(src);
+    });
+    var slideshow = document.querySelector('.slideshow');
+    slideshow.querySelector('.slideshow-slides').innerHTML = strHtml;
 }
 
 // Util functions.
@@ -114,17 +75,10 @@ function addClass (element, className)
 {
     element.className += ' ' + className;
 }
-
 function removeClass (element, className)
 {
     element.className = element.className.replace(new RegExp('(?:^|\\s)'+ className + '(?:\\s|$)'), ' ');
 }
-
-function hasClass (element, className)
-{
-    return element.className.match(/(?:^|\s)className(?!\S)/);
-}
-
 function on(element, types, listener)
 {
     var arrTypes = types.split(' ');
@@ -134,50 +88,3 @@ function on(element, types, listener)
         element.addEventListener(type, listener);
     }
 }
-
-function bounds(element) 
-{
-    return element.getBoundingClientRect();
-}
-
-function viewportHeight() 
-{
-    return document.documentElement.clientHeight;
-}
-
-function pageTopOffset() 
-{
-    var doc = document.documentElement;
-    return {
-        x : (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0),
-        y : (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0)
-    };
-}
-
-function pageBottomOffset() 
-{
-    return pageTopOffset().y + viewportHeight();
-}
-
-function offset(element) 
-{
-    return pageTopOffset().y + bounds(element).top;
-}
-
-function debounce(func, wait, immediate) 
-{
-    var timeout;
-    return function() 
-    {
-        var me = this, args = arguments;
-        var later = function() 
-        {
-            timeout = null;
-            if (!immediate) func.apply(me, args);
-        };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait || 250);
-        if (callNow) func.apply(me, args);
-    };
-};
